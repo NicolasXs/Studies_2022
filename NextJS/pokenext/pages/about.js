3e1dbb67e350d6df54be5a3b691f04031f70ac6f
Nicolas Lacerda <nicolaasxs@gmail.com>
2022-11-16 23:59:18 -0300

    Curso Next.js: Criando página no sistema - #21

---
+++ NextJS/pokenext/pages/about.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "../styles/about.module.css";
+import charizard from "../public/images/charizard.png";
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae obcaecati similique quam quibusdam ut ad quas, ex fugit enim placeat odio, dicta labore sit nostrum blanditiis nam? Nemo, similique quos?
+      </p>
+      <Image src={charizard} width={300} height={300} alt="Charizard" />
+    </div>
+  );
+}
