424afa8867a113bc100408b1b6fb80ba267a1453
Nicolas Lacerda <nicolaasxs@gmail.com>
2022-11-18 23:01:16 -0300

    Curso Next.js: Finalizando página de Pokemon - #26

--- NextJS/pokenext/components/Navbar.js
+++ NextJS/pokenext/components/Navbar.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../../public/images/pokeball.png";
+import logo from "../public/images/pokeball.png";
 
-import styles from '../../styles/Navbar.module.css'
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
+      <Link className={styles.logo} href="/">
         <Image src={logo} width={30} height={30} alt="PokeNext" />
         <h1>PokeNext</h1>
-      </div>
+      </Link>
       <ul className={styles.link_items}>
         <li>
           <Link href="/">Home</Link>
